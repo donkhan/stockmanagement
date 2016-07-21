ed013a560a48c066e011513935d591cc9dfa3b24
donkhan <=donkhan@yahoo.com>
2016-07-21 10:27:10 +0530

    Sold 20 Tata Steel

--- src/main/java/commissioncalculators/TaxUtil.java
+++ src/main/java/commissioncalculators/TaxUtil.java
@@ -8,7 +8,6 @@ public class TaxUtil {
 	public static boolean cutOff(Date d, GregorianCalendar c2){
 		GregorianCalendar cal = new GregorianCalendar();
 		cal.setTime(d);
-		//System.out.println(" " + c2.getTime() + "  " + cal.getTime() );
 		return cal.before(c2);
 	}
 }
